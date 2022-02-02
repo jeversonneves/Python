8b7683bfe49bb226ea2e09a43f418886bfc6a865
jeversonneves <93106179+jeversonneves@users.noreply.github.com>
2022-02-02 09:06:47 -0300

    Create Adivinhação.py

---
+++ exercicios/Adivinhação.py
@@ -0,0 +1,23 @@
+from random import randint
+
+computer = randint(0, 10)
+
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi? ')
+
+acertou = False
+palpites = 0
+
+while not acertou:
+    player = int(input('Qual é seu palpite? '))
+    palpites += 1
+
+    if player == computer:
+        acertou = True
+    else:
+      if player < computer:
+        print('Mais... Tente mais uma vez.')
+      elif player > computer:
+        print('Menos... Tente mais uma vez')
+
+print('Acertou com {} tentativas. Parabéns!'.format(palpites))
